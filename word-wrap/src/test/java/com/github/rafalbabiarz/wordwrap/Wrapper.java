ba5305f133b53311476b6c4f57b3cd8672e30b3c
Rafal Babiarz <rafal.babiarz@gmail.com>
2016-01-13 21:41:34 +0100

    Return same string when wrap is called

--- word-wrap/src/test/java/com/github/rafalbabiarz/wordwrap/Wrapper.java
+++ word-wrap/src/test/java/com/github/rafalbabiarz/wordwrap/Wrapper.java
@@ -1,10 +1,16 @@
 package com.github.rafalbabiarz.wordwrap;
 
 public class Wrapper {
-
     static final String INVALID_COLUMN_NUMBER_ERROR_MESSAGE = "Column number must be bigger than 0";
 
     public static String wrap(String string, int columnNumber) {
-        throw new IllegalArgumentException(INVALID_COLUMN_NUMBER_ERROR_MESSAGE);
+        if (columnNumber < 1) {
+            throw new IllegalArgumentException(INVALID_COLUMN_NUMBER_ERROR_MESSAGE);
+        }
+        return new Wrapper().wrap(string);
+    }
+
+    private String wrap(String toWrap) {
+        return toWrap;
     }
 }
--- word-wrap/src/test/java/com/github/rafalbabiarz/wordwrap/WrapperTest.java
+++ word-wrap/src/test/java/com/github/rafalbabiarz/wordwrap/WrapperTest.java
@@ -5,11 +5,19 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import static com.github.rafalbabiarz.wordwrap.Wrapper.wrap;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WrapperTest {
     private static final String ANY_STRING = "any string";
 
     @Rule public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void returnsSameStringWhenItsShorterThanColumnNumber() {
+        String wrapped = wrap(ANY_STRING, ANY_STRING.length() + 1);
+
+        assertThat(wrapped).isEqualTo(ANY_STRING);
+    }
 
     @Test
     public void throwsInvalidArgumentExceptionWhenColumnNumberIsSmallerThanOne() {
